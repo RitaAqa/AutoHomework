61de866de2ffe1c6d479474246bda67a5ccd3ba3
lymos <margarita.leonidova@gmail.com>
2022-03-30 20:58:10 +0300

    first change

---
+++ src/main/java/GetRequest.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class GetRequest{
+
+    public static void main(String args[])  throws IOException {
+
+        URL url = new URL("https://api.github.com/users/google");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+
+}
